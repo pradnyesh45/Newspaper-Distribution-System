3995f516057fe692b7e706f58b0868869f1b8672
pradnyesh45 <pradnyeshaglawe12@gmail.com>
2020-05-26 00:34:57 +0530

    added ejs view engine and rendered the home page

--- index.js
+++ index.js
@@ -4,4 +4,7 @@ const port = 8000;
 
 app.use('/', require('./routes/index'));
 
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
 app.listen(port, () => console.log(`Server running on port: ${port}`));
