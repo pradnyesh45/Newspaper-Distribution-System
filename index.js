f741fd76c6249ad00f31e209c29f0cd401c2ea3d
pradnyesh45 <pradnyeshaglawe12@gmail.com>
2020-05-26 00:04:18 +0530

    Added routes and controllers for home and users profile page

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+app.use('/', require('./routes/index'));
+
 app.listen(port, () => console.log(`Server running on port: ${port}`));
